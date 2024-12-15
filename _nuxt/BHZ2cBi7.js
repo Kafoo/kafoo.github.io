6ebebdf28f2771ea8d97a057b3aa5688f60ea772
Kafoo <ant.guillard@gmail.com>
2024-12-15 18:08:38 +0100

    npm update + changed api

--- _nuxt/BHZ2cBi7.js
+++
@@ -1,1 +0,0 @@
-import{k as e,a6 as o,a5 as r}from"./Dl9LHTZm.js";import{_ as t}from"./B20Iwgmb.js";const a=e({props:{},data(){return{}}});function n(s,c,p,_,i,d){return r(),o("hr")}const l=t(a,[["render",n],["__scopeId","data-v-48a2e7c2"]]);export{l as H};
